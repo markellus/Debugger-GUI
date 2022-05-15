039b2b18a565886287c08c8b9016121fb98465ff
Marcel Bulla <gmx@marcel-bulla.de>
2022-05-15 14:50:07 +0200

    Implemented MainWindow structure

---
+++ Debugger-GUI/Source/GUI/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿/*
+ * Copyright 2022 Marcel Bulla
+ * 
+ * This file is part of Debugger-GUI.
+ * 
+ * Debugger-GUI is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+ * 
+ * Foobar is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU General Public License along
+ * with Foobar. If not, see http://www.gnu.org/licenses/.
+ */
+
+using AdonisUI.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Debugger.Source.GUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : AdonisWindow
+    {
+        public MainWindow()
+        {
+            //InitializeComponent();
+        }
+
+        protected override void OnInitialized(EventArgs e)
+        {
+            base.OnInitialized(e);
+        }
+    }
+}
